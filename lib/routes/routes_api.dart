b8465b4155c0d67f1ab0b2e2c72dc0f3b8adfa3f
Jeancarlos  Carvajal <jeancarloscarvajalavila@gmail.com>
2022-10-04 20:07:04 -0400

    Refactorizado el uso de la apiKEY II

--- lib/routes/routes_api.dart
+++ lib/routes/routes_api.dart
@@ -2,11 +2,11 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class RoutesAPI {
 
-  static String? get apiKEY => dotenv.env['API_KEY'];
-  static String get language => 'en-US';
-  static String get baseURL => 'api.themoviedb.org';
-  static String get popularEndPoint => '/3/movie/popular';
+  static String? get apiKEY          => dotenv.env['API_KEY'];
+  static String  get language        => 'en-US';
+  static String  get baseURL         => 'api.themoviedb.org';
+  static String  get popularEndPoint => '/3/movie/popular';
+  static String  get moviesByActor   => '/3/search/person';
   static String detailsEndPont({ required String movieId }) => '/3/movie/$movieId/credits';
-  static String get moviesByActor => '/3/search/person';
 
 }
